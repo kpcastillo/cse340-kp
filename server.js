a0f838062b426bd02abc373b9ed062eccbe9cb55
kpcastillo <krn_iveth09@hotmail.com>
2025-10-28 14:14:24 -0700

    change home name

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 // Index route
 app.get("/", function (req, res){
-  res.render("index", {title: "Home"})
+  res.render("index", {title: "Karen's Homepage"})
 } )
 
 /* ***********************
