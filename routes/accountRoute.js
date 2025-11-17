e25d681fb0d6f60a58cce5e0c9072efd209c19c2
kpcastillo <krn_iveth09@hotmail.com>
2025-11-17 14:58:45 -0800

    Registration View

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,10 +1,13 @@
 //Needed resources
 const express = require("express")
 const router = new express.Router()
-const indexUtilities= require("../utilities/index")
+const Utilities= require("../utilities/index")
 const accountController = require("../controllers/accountController")
 
 //Route to build the account login page
-router.get("/login/", indexUtilities.handleErrors(accountController.buildLogin))
+router.get("/login/", Utilities.handleErrors(accountController.buildLogin))
+
+//Registration route to build the account registration page
+router.get("/register/", Utilities.handleErrors(accountController.buildRegister))
 
 module.exports = router;
