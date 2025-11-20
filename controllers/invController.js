b5c32de0e5685b5d663f3e6b626126b61d86544b
kpcastillo <krn_iveth09@hotmail.com>
2025-11-20 06:51:18 -0800

    Server validation activity

--- controllers/invController.js
+++ controllers/invController.js
@@ -16,6 +16,7 @@ invCont.buildByClassificationId = async function (req, res, next) {
     title: className + " vehicles",
     nav,
     grid,
+    errors: null
   })
 }
 
@@ -31,6 +32,7 @@ invCont.buildByItemId = async function (req, res, next) {
     title: data.inv_make + " " + data.inv_model,
     nav,
     itemView,
+    errors: null
   })
 }
 module.exports = invCont
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router()
 const Utilities= require("../utilities/index")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
 
 //Route to build the account login page
 router.get("/login/", Utilities.handleErrors(accountController.buildLogin))
@@ -10,6 +11,10 @@ router.get("/login/", Utilities.handleErrors(accountController.buildLogin))
 //Registration route to build the account registration page
 router.get("/register/", Utilities.handleErrors(accountController.buildRegister))
 
-router.post('/register', Utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    '/register', regValidate.registationRules(),
+    regValidate.checkRegData,
+    Utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
